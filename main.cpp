a7544ae35368f3b335fab11478e47164e5df4159
ddim7664 <77564227+ddim7664@users.noreply.github.com>
2022-06-16 13:36:19 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <swithclass>
+
+ public static void first_argument()
+{
+    int VAMPIRE_activation, VAMPIRE_attack, VAMPIRE_skill, VAMPIRE_behavior;
+    int BARD_activation, BARD_attack, BARD_skill, BARD_behavior;
+    int ARCHER_activation, ARCHER_attack, ARCHER_skill, ARCHER_behavior;
+    int SAVIOR_activation, SAVIOR_attack, SAVIOR_skill, SAVIOR_behavior;
+
+    int MONEY, ITEM, SKILL, STATUS;
+
+
+    bool TRUE:
+    switch
+    {
+        case1:
+
+    }
+}
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+
+}
